a892b7c831b3d2f62a174c601e891ac78847a7d8
sadik5397@gmail.com </student/>
2023-01-22 15:41:25 +0600

    + First Loading 24% Faster

--- lib/views/test.dart
+++ lib/views/test.dart
@@ -36,7 +36,9 @@ class _ExportDataState extends State<ExportData> {
         showSnackBar(context: context, label: result["message"]);
         setState(() => allBuildingResult = result["data"]);
       } else {
-        showError(context: context, label: result["message"][0].toString().length == 1 ? result["message"].toString() : result["message"][0].toString());
+        showError(context: context, label: result["message"][0]
+            .toString()
+            .length == 1 ? result["message"].toString() : result["message"][0].toString());
       }
     } on Exception catch (e) {
       showError(context: context, label: e.toString());
@@ -52,7 +54,9 @@ class _ExportDataState extends State<ExportData> {
         showSnackBar(context: context, label: result["message"]);
         setState(() => allUserResult = result["data"]);
       } else {
-        showError(context: context, label: result["message"][0].toString().length == 1 ? result["message"].toString() : result["message"][0].toString());
+        showError(context: context, label: result["message"][0]
+            .toString()
+            .length == 1 ? result["message"].toString() : result["message"][0].toString());
       }
     } on Exception catch (e) {
       showError(context: context, label: e.toString());
@@ -61,40 +65,85 @@ class _ExportDataState extends State<ExportData> {
 
   //Function
   Future<void> allBuildingsExportToExcel() async {
-    final stopwatch = Stopwatch()..start();
+    final stopwatch = Stopwatch()
+      ..start();
     await readAllBuilding(accessToken: accessToken);
     final excel = Excel.createExcel();
     final Sheet sheet = excel[excel.getDefaultSheet()!];
     //region header
-    sheet.cell(CellIndex.indexByColumnRow(columnIndex: 0, rowIndex: 0)).value = "Building Serial";
-    sheet.cell(CellIndex.indexByColumnRow(columnIndex: 1, rowIndex: 0)).value = "Building Unique ID";
-    sheet.cell(CellIndex.indexByColumnRow(columnIndex: 2, rowIndex: 0)).value = "Building Name";
-    sheet.cell(CellIndex.indexByColumnRow(columnIndex: 3, rowIndex: 0)).value = "Building Address";
-    sheet.cell(CellIndex.indexByColumnRow(columnIndex: 4, rowIndex: 0)).value = "Approval Status";
-    sheet.cell(CellIndex.indexByColumnRow(columnIndex: 5, rowIndex: 0)).value = "Contact Person Name";
-    sheet.cell(CellIndex.indexByColumnRow(columnIndex: 6, rowIndex: 0)).value = "Contact Person Phone";
-    sheet.cell(CellIndex.indexByColumnRow(columnIndex: 7, rowIndex: 0)).value = "Contact Person Email";
-    sheet.cell(CellIndex.indexByColumnRow(columnIndex: 8, rowIndex: 0)).value = "Total Flats";
-    sheet.cell(CellIndex.indexByColumnRow(columnIndex: 9, rowIndex: 0)).value = "Building Picture";
-    sheet.cell(CellIndex.indexByColumnRow(columnIndex: 10, rowIndex: 0)).value = "Access Point Email";
+    sheet
+        .cell(CellIndex.indexByColumnRow(columnIndex: 0, rowIndex: 0))
+        .value = "Building Serial";
+    sheet
+        .cell(CellIndex.indexByColumnRow(columnIndex: 1, rowIndex: 0))
+        .value = "Building Unique ID";
+    sheet
+        .cell(CellIndex.indexByColumnRow(columnIndex: 2, rowIndex: 0))
+        .value = "Building Name";
+    sheet
+        .cell(CellIndex.indexByColumnRow(columnIndex: 3, rowIndex: 0))
+        .value = "Building Address";
+    sheet
+        .cell(CellIndex.indexByColumnRow(columnIndex: 4, rowIndex: 0))
+        .value = "Approval Status";
+    sheet
+        .cell(CellIndex.indexByColumnRow(columnIndex: 5, rowIndex: 0))
+        .value = "Contact Person Name";
+    sheet
+        .cell(CellIndex.indexByColumnRow(columnIndex: 6, rowIndex: 0))
+        .value = "Contact Person Phone";
+    sheet
+        .cell(CellIndex.indexByColumnRow(columnIndex: 7, rowIndex: 0))
+        .value = "Contact Person Email";
+    sheet
+        .cell(CellIndex.indexByColumnRow(columnIndex: 8, rowIndex: 0))
+        .value = "Total Flats";
+    sheet
+        .cell(CellIndex.indexByColumnRow(columnIndex: 9, rowIndex: 0))
+        .value = "Building Picture";
+    sheet
+        .cell(CellIndex.indexByColumnRow(columnIndex: 10, rowIndex: 0))
+        .value = "Access Point Email";
     //endregion
     for (int i = 0; i < allBuildingResult.length; i++) {
       //region data
-      sheet.cell(CellIndex.indexByColumnRow(columnIndex: 0, rowIndex: i + 1)).value = allBuildingResult[i]["buildingId"].toString();
-      sheet.cell(CellIndex.indexByColumnRow(columnIndex: 1, rowIndex: i + 1)).value = allBuildingResult[i]["uniqueId"].toString();
-      sheet.cell(CellIndex.indexByColumnRow(columnIndex: 2, rowIndex: i + 1)).value = allBuildingResult[i]["buildingName"].toString();
-      sheet.cell(CellIndex.indexByColumnRow(columnIndex: 3, rowIndex: i + 1)).value = allBuildingResult[i]["address"].toString();
-      sheet.cell(CellIndex.indexByColumnRow(columnIndex: 4, rowIndex: i + 1)).value = allBuildingResult[i]["approvalStatus"].toString();
-      sheet.cell(CellIndex.indexByColumnRow(columnIndex: 5, rowIndex: i + 1)).value = allBuildingResult[i]["createdBy"] == null ? "" : allBuildingResult[i]["createdBy"]["name"].toString();
-      sheet.cell(CellIndex.indexByColumnRow(columnIndex: 6, rowIndex: i + 1)).value = allBuildingResult[i]["createdBy"] == null
+      sheet
+          .cell(CellIndex.indexByColumnRow(columnIndex: 0, rowIndex: i + 1))
+          .value = allBuildingResult[i]["buildingId"].toString();
+      sheet
+          .cell(CellIndex.indexByColumnRow(columnIndex: 1, rowIndex: i + 1))
+          .value = allBuildingResult[i]["uniqueId"].toString();
+      sheet
+          .cell(CellIndex.indexByColumnRow(columnIndex: 2, rowIndex: i + 1))
+          .value = allBuildingResult[i]["buildingName"].toString();
+      sheet
+          .cell(CellIndex.indexByColumnRow(columnIndex: 3, rowIndex: i + 1))
+          .value = allBuildingResult[i]["address"].toString();
+      sheet
+          .cell(CellIndex.indexByColumnRow(columnIndex: 4, rowIndex: i + 1))
+          .value = allBuildingResult[i]["approvalStatus"].toString();
+      sheet
+          .cell(CellIndex.indexByColumnRow(columnIndex: 5, rowIndex: i + 1))
+          .value = allBuildingResult[i]["createdBy"] == null ? "" : allBuildingResult[i]["createdBy"]["name"].toString();
+      sheet
+          .cell(CellIndex.indexByColumnRow(columnIndex: 6, rowIndex: i + 1))
+          .value = allBuildingResult[i]["createdBy"] == null
           ? ""
           : (allBuildingResult[i]["createdBy"]["phone"] == "00000000000" || allBuildingResult[i]["createdBy"]["phone"] == "___________")
-              ? ""
-              : allBuildingResult[i]["createdBy"]["phone"].toString();
-      sheet.cell(CellIndex.indexByColumnRow(columnIndex: 7, rowIndex: i + 1)).value = allBuildingResult[i]["createdBy"] == null ? "" : allBuildingResult[i]["createdBy"]["email"].toString();
-      sheet.cell(CellIndex.indexByColumnRow(columnIndex: 8, rowIndex: i + 1)).value = allBuildingResult[i]["flats"].length;
-      sheet.cell(CellIndex.indexByColumnRow(columnIndex: 9, rowIndex: i + 1)).value = allBuildingResult[i]["photo"] == null ? "" : '$baseUrl/photos/${allBuildingResult[i]["photo"]}';
-      sheet.cell(CellIndex.indexByColumnRow(columnIndex: 10, rowIndex: i + 1)).value = allBuildingResult[i]["guard"] == null ? "" : allBuildingResult[i]["guard"]["email"].toString();
+          ? ""
+          : allBuildingResult[i]["createdBy"]["phone"].toString();
+      sheet
+          .cell(CellIndex.indexByColumnRow(columnIndex: 7, rowIndex: i + 1))
+          .value = allBuildingResult[i]["createdBy"] == null ? "" : allBuildingResult[i]["createdBy"]["email"].toString();
+      sheet
+          .cell(CellIndex.indexByColumnRow(columnIndex: 8, rowIndex: i + 1))
+          .value = allBuildingResult[i]["flats"].length;
+      sheet
+          .cell(CellIndex.indexByColumnRow(columnIndex: 9, rowIndex: i + 1))
+          .value = allBuildingResult[i]["photo"] == null ? "" : '$baseUrl/photos/${allBuildingResult[i]["photo"]}';
+      sheet
+          .cell(CellIndex.indexByColumnRow(columnIndex: 10, rowIndex: i + 1))
+          .value = allBuildingResult[i]["guard"] == null ? "" : allBuildingResult[i]["guard"]["email"].toString();
       //endregion
       setState(() => allBuildingResultProgress = allBuildingResultProgress + 100 / allBuildingResult.length);
     }
@@ -104,29 +153,58 @@ class _ExportDataState extends State<ExportData> {
   }
 
   Future<void> allUsersExportToExcel() async {
-    final stopwatch = Stopwatch()..start();
+    final stopwatch = Stopwatch()
+      ..start();
     await readAllUser(accessToken: accessToken);
     final excel = Excel.createExcel();
     final Sheet sheet = excel[excel.getDefaultSheet()!];
     //region header
-    sheet.cell(CellIndex.indexByColumnRow(columnIndex: 0, rowIndex: 0)).value = "User ID";
-    sheet.cell(CellIndex.indexByColumnRow(columnIndex: 1, rowIndex: 0)).value = "Full Name";
-    sheet.cell(CellIndex.indexByColumnRow(columnIndex: 2, rowIndex: 0)).value = "Email";
-    sheet.cell(CellIndex.indexByColumnRow(columnIndex: 3, rowIndex: 0)).value = "Phone";
-    sheet.cell(CellIndex.indexByColumnRow(columnIndex: 4, rowIndex: 0)).value = "Gender";
-    sheet.cell(CellIndex.indexByColumnRow(columnIndex: 5, rowIndex: 0)).value = "Profile Picture";
-    sheet.cell(CellIndex.indexByColumnRow(columnIndex: 6, rowIndex: 0)).value = "Hi Society Role";
+    sheet
+        .cell(CellIndex.indexByColumnRow(columnIndex: 0, rowIndex: 0))
+        .value = "User ID";
+    sheet
+        .cell(CellIndex.indexByColumnRow(columnIndex: 1, rowIndex: 0))
+        .value = "Full Name";
+    sheet
+        .cell(CellIndex.indexByColumnRow(columnIndex: 2, rowIndex: 0))
+        .value = "Email";
+    sheet
+        .cell(CellIndex.indexByColumnRow(columnIndex: 3, rowIndex: 0))
+        .value = "Phone";
+    sheet
+        .cell(CellIndex.indexByColumnRow(columnIndex: 4, rowIndex: 0))
+        .value = "Gender";
+    sheet
+        .cell(CellIndex.indexByColumnRow(columnIndex: 5, rowIndex: 0))
+        .value = "Profile Picture";
+    sheet
+        .cell(CellIndex.indexByColumnRow(columnIndex: 6, rowIndex: 0))
+        .value = "Hi Society Role";
     //endregion
     for (int i = 0; i < allUserResult.length; i++) {
       //region data
-      sheet.cell(CellIndex.indexByColumnRow(columnIndex: 0, rowIndex: i + 1)).value = allUserResult[i]["userId"].toString();
-      sheet.cell(CellIndex.indexByColumnRow(columnIndex: 1, rowIndex: i + 1)).value = allUserResult[i]["name"].toString();
-      sheet.cell(CellIndex.indexByColumnRow(columnIndex: 2, rowIndex: i + 1)).value = allUserResult[i]["email"].toString();
-      sheet.cell(CellIndex.indexByColumnRow(columnIndex: 3, rowIndex: i + 1)).value =
-          (allUserResult[i]["phone"] == "00000000000" || allUserResult[i]["phone"] == "___________") ? "" : allUserResult[i]["phone"].toString();
-      sheet.cell(CellIndex.indexByColumnRow(columnIndex: 4, rowIndex: i + 1)).value = allUserResult[i]["gender"].toString();
-      sheet.cell(CellIndex.indexByColumnRow(columnIndex: 5, rowIndex: i + 1)).value = allUserResult[i]["photo"] == null ? "" : '$baseUrl/photos/${allUserResult[i]["photo"]}';
-      sheet.cell(CellIndex.indexByColumnRow(columnIndex: 6, rowIndex: i + 1)).value = allUserResult[i]["role"] == null ? "" : allUserResult[i]["role"]["role"].toString();
+      sheet
+          .cell(CellIndex.indexByColumnRow(columnIndex: 0, rowIndex: i + 1))
+          .value = allUserResult[i]["userId"].toString();
+      sheet
+          .cell(CellIndex.indexByColumnRow(columnIndex: 1, rowIndex: i + 1))
+          .value = allUserResult[i]["name"].toString();
+      sheet
+          .cell(CellIndex.indexByColumnRow(columnIndex: 2, rowIndex: i + 1))
+          .value = allUserResult[i]["email"].toString();
+      sheet
+          .cell(CellIndex.indexByColumnRow(columnIndex: 3, rowIndex: i + 1))
+          .value =
+      (allUserResult[i]["phone"] == "00000000000" || allUserResult[i]["phone"] == "___________") ? "" : allUserResult[i]["phone"].toString();
+      sheet
+          .cell(CellIndex.indexByColumnRow(columnIndex: 4, rowIndex: i + 1))
+          .value = allUserResult[i]["gender"].toString();
+      sheet
+          .cell(CellIndex.indexByColumnRow(columnIndex: 5, rowIndex: i + 1))
+          .value = allUserResult[i]["photo"] == null ? "" : '$baseUrl/photos/${allUserResult[i]["photo"]}';
+      sheet
+          .cell(CellIndex.indexByColumnRow(columnIndex: 6, rowIndex: i + 1))
+          .value = allUserResult[i]["role"] == null ? "" : allUserResult[i]["role"]["role"].toString();
       //endregion
       setState(() => allUserResultProgress = allUserResultProgress + 100 / allUserResult.length);
     }
